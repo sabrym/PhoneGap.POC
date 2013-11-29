0f87649d928d3cc3529ec2f25da133bdbf079dc0
Sabry  Moulana <SabryM@99x.lk>
2013-11-29 17:27:32 +0530

    Added the kendo test app

---
+++ KendoTestApp/assets/www/js/current.js
@@ -0,0 +1,97 @@
+    var Current = (function (Context) {
+        
+        function Current (FB) {
+            this.applicationId = "625372577501704"; 
+            if (FB == undefined) 
+            {
+                alert("I have been undefined");
+            }
+            else
+            {
+                this.fb = FB;
+
+            }
+        }
+
+        Current.prototype.subscribeForFBEvents = function() {
+            this.fb.Event.subscribe('auth.login', function(response) {
+             alert('auth.login event');
+         });
+
+            this.fb.Event.subscribe('auth.logout', function(response) {
+             alert('auth.logout event');
+         });
+
+            this.fb.Event.subscribe('auth.sessionChange', function(response) {
+             alert('auth.sessionChange event');
+         });
+
+            this.fb.Event.subscribe('auth.statusChange', function(response) {
+             alert('auth.statusChange event');
+         });
+        };
+
+        Current.prototype.initFb = function() {
+         if (this.fb !== undefined) 
+         {
+            try {
+               this.fb.init({
+                   appId: this.applicationId,
+                   nativeInterface: CDV.FB,
+                   useCachedDialogs: false,
+               });
+           } catch (e) {
+               alert(e);
+           }
+       }
+
+       Current.prototype.loginUser = function() {
+          this.fb.login(
+               function(response) {
+                 if (response.session) {
+                   // alert('logged in');
+                   // window.kendoMobileApplication("#secondview");
+                   return true;
+                 } else {
+                   alert('Facebook login Failed');
+                   return false;
+                 }
+               },
+               { scope: "email" }
+               );
+       };
+
+       Current.prototype.getMyFriends = function(numberOfFriends) {
+         var friendIDs = [];
+         var counter = 0;
+         var fdata;
+         FB.api('/me/friends', { fields: 'id, name, picture' },  function(response) {
+             if (response.error) {
+               alert(JSON.stringify(response.error));
+           } else {
+               var data = document.getElementById('data');
+               fdata=response.data;
+               console.log("fdata: "+fdata);
+               response.data.forEach(function(item) {
+                 var d = document.createElement('div');
+                    // alert("User url :" + item.picture.data.url);
+                    d.innerHTML = "<img src='"+item.picture.data.url+"'/>"+item.name;
+                    data.appendChild(d);
+
+                    friendIDs.push(item.name);
+                    console.log("The intermediate array :" + friendIDs);
+                    counter++;
+                });
+           }
+       });
+
+         this.retrievedFriends = friendIDs;    
+     };
+
+     Current.prototype.getRandomFriend = function() {
+        alert(this.retrievedFriends.length);
+    };
+};
+
+return Current;
+})();
