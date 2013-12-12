dc7e88583d8483388328deeb17dc67d94846ab7b
Sabry  Moulana <SabryM@99x.lk>
2013-12-12 12:39:29 +0530

    Updated
    Fixed the friends list view deselect issue

--- KendoTestApp/assets/www/js/Meetup.js
+++ KendoTestApp/assets/www/js/Meetup.js
@@ -28,26 +28,6 @@ var Meetup = (function (Context) {
 		return selectedFriends;
 	}
 
-	// // ApplicationUser.prototype.addFriendsList = function(friendsList) {
-	// // 	this.friendsIDs = friendsList;
-	// // }
-
-	// Meetup.prototype.getMyFriends = function(friendsArray) {
-	// 	alert("In here" + friendsArray.length);
-	// 	// check if the list returned from the server is valid
-	// 	// once valid, add to the object model for later use
-	// 	if (friendsArray !== undefined) 
-	// 	{
-	// 		this.friendsList = new Array();
-	// 		for (var i = 0; i < friendsArray.length; i++) {
-	// 			var currentFriend = new FacebookFriend(friendsArray[i].name, friendsArray[i].picture.data.url);				
-	// 			this.friendsList.push(currentFriend);
-	// 		}
-	// 	}
-
-	// 	alert("In here" + this.friendsList.length);
-	// }
-
 	return Meetup;
 
 })();
